63b410a9b650a0acddcd9695c054374afa7f3786
vitalik061981 <vitalik061981@mail.ru>
2022-08-23 10:36:44 +0300

    add Hometask13.py

---
+++ Hometask13.py
@@ -0,0 +1,23 @@
+# Hometask_13_3
+# Напишите функцию которая
+# принимает на вход список
+# целых чисел, удаляет из него
+# все нечётные значения, а
+# чётные нацело делит на два.
+# In: 852 85 784 58
+# Out: [426, 382, 29]
+nums = [int(i) for i in input().split()]
+
+def even(nums):
+
+
+    a = []
+
+    for i in nums:
+        if i % 2 == 0:
+            i = i / 2
+            a.append(int(i))
+    return a
+
+
+print(even(nums))
