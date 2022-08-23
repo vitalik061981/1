1b5ec866e876d2945d03847fc01ca03191f5b95c
vitalik061981 <vitalik061981@mail.ru>
2022-08-23 14:38:36 +0300

    add Hometask_13_1.py

--- Hometask_13_1.py
+++ Hometask_13_1.py
@@ -6,15 +6,18 @@
 # чётные нацело делит на два.
 # In: 852 85 784 58
 # Out: [426, 382, 29]
+nums = [int(i) for i in input().split()]
 
 def even(nums):
 
-    nums = [int(i) for i in input().split()]
+
     a = []
 
     for i in nums:
         if i % 2 == 0:
             i = i / 2
             a.append(int(i))
+    return a
 
-print(even(a))
+
+print(even(nums))
